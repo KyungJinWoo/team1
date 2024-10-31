9f3f80c81c083b280b05eadc150ab50baf263908
경진우 <wlsdn6746@naver.com>
2024-10-31 10:44:19 +0900

    Update app16.py

--- app16.py
+++ app16.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # 제목 설정
-st.title("Video Player App")
+st.title("비디오 사물 검출 앱")
 
 # 파일 업로드
 uploaded_file = st.file_uploader("Upload a video file", type=["mp4", "mov", "avi"])
@@ -11,4 +11,4 @@ if uploaded_file is not None:
     # 비디오 플레이어
     st.video(uploaded_file)
 else:
-    st.write("Please upload a video file to play.")
+    st.write("재생하고 싶은 비디오 파일을 업로드하세요.")
